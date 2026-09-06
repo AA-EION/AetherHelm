1ba11c5eb05a0a5ebd1963b8f0bdb40489d53581
EION <31860825+AA-EION@users.noreply.github.com>
2026-09-06 12:04:26 -0500

    chore: remove accidentally committed build-mingw artifacts and ignore build directories

--- build-mingw/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /helm
-# Build directory: /helm/build-mingw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AetherHelmTestSuite]=] "/helm/build-mingw/aetherhelm-tests_artefacts/Release/aetherhelm-tests.exe")
-set_tests_properties([=[AetherHelmTestSuite]=] PROPERTIES  _BACKTRACE_TRIPLES "/helm/CMakeLists.txt;391;add_test;/helm/CMakeLists.txt;0;")
-subdirs("_deps/juce-build")
